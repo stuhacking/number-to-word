f70a9329a41e755521168dac2668b6f8c0e836f7
shacking <stuart.hacking@oracle.com>
2015-07-23 16:59:17 +0100

    Ensure output string is trimmed.

--- src/number_to_word/core.clj
+++ src/number_to_word/core.clj
@@ -1,4 +1,5 @@
-(ns number-to-word.core)
+(ns number-to-word.core
+  (:require [clojure.string :as s]))
 
 (def ^:const numbers
   "Table of primitive numerals as words."
@@ -62,26 +63,20 @@ of size N."
          l
          (recur (drop n s) (conj l (pad-group (reverse (take n s)) n)))))))
 
-;; Check if X is both non nil and > 0
-(defn num? [x]
-  (and x (< 0 x)))
-
-(defn group->word [[fst snd thd] level]
+(defn group->word [[fst snd thd]]
   (let [words (atom [])]
-    (when (num? fst)
+    (when (< 0 fst)
       (swap! words conj (numbers fst) "hundred")
-      (when (or (num? snd) (num? thd))
+      (when (or (< 0 snd) (< 0 thd))
         (swap! words conj "and")))
-    (if (and (num? snd) (= 1 snd))
+    (if (and (= 1 snd))
       (swap! words conj (numbers (+ thd (* 10 snd))))
       (do 
-        (when (num? snd)
+        (when (< 0 snd)
           (swap! words conj (numbers (* 10 snd))))
-        (when (num? thd)
+        (when (< 0 thd)
           (swap! words conj (numbers thd)))))
-    (when (num? (count @words))
-      (swap! words conj (powers-of-ten level)))
-    (clojure.string/join " " (filter #(not (= "" %)) @words))))
+    (s/join " " (filter (complement s/blank?) @words))))
 
 (defn number->word
   "Convert an arabic number into readable English text."
@@ -94,10 +89,11 @@ of size N."
         ;; Done
         (if (empty? words)
           "zero"
-          (clojure.string/join ", " words))
+          (s/trim (s/join ", " words)))
         ;; Recur
-        (let [word (group->word (first num-groups)
-                    level)]
+        (let [simple-number (group->word (first num-groups))
+              word (if (empty? simple-number) simple-number
+                       (s/join " " [simple-number (powers-of-ten level)]))]
           (recur (rest num-groups)
                  (if (not (= "" word))
                    (conj words word)
