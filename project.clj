013dccc46c04131c8f1ceae2ed50fc60ab8b26c2
Stuart Hacking <stuhacking@gmail.com>
2016-08-30 17:19:05 +0100

    Update Project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject number-to-word "0.1.0-SNAPSHOT"
   :description "Module to convert arabic numbers into readable text."
-  :url "tbc..."
+  :url "https://github.com/stuhacking/number-to-word"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
